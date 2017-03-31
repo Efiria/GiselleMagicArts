0d0b01aa567b796330f620dea394a258cc595369
Efiria <bruno.doucet@viacesi.fr>
2017-03-31 10:37:35 +0200

    ScriptIngredient

---
+++ Conception/InsertionValeur/InsertionIngredient.sql
@@ -0,0 +1,18 @@
+
+START TRANSACTION;
+INSERT INTO ingredient (NomIngredient, Fraicheur, PrixIngredient) 
+VALUES ('Aigue-marine','56','20'),
+('Alexandrite','464','20'),
+('Azurite','63','15'),
+('Chrysobéryl','74','14'),
+('Grenat rouge','12','15'),
+('Grenat violet','66','12'),
+('Irtios','64','13'),
+('Ivoire','984','12'),
+('Lapis-Lazuli','44','16'),
+('Néphrite','498','13'),
+('Orprase','49','12'),
+('Péridot','16','15'),
+('Perle dorée','56','18'),
+('Perle noire','748','12');
+COMMIT;
