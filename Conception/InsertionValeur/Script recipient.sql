be027a794f1882e0638285ffe6ed2a7a66be911b
Efiria <bruno.doucet@viacesi.fr>
2017-04-04 15:57:28 +0200

    Delete Script recipient.sql

--- Conception/InsertionValeur/Script recipient.sql
+++
@@ -1,8 +0,0 @@
-START TRANSACTION;
-INSERT INTO `recipient` (`TypeRecipient`,`PrixRecipient`,`Volume`) VALUES
-('Fiole','5','1'),
-('Tube','22','2'),
-('Pot','34','5'),
-('Amphore','70','5'),
-('Fût','126','10');
-COMMIT;
