057b9e09c60831916e4e020b0df6e4e26048386c
Efiria <bruno.doucet@viacesi.fr>
2017-04-05 09:24:29 +0200

    Add files via upload

--- Conception/InsertionValeur/ScripInsertionTotal.sql
+++ Conception/InsertionValeur/ScripInsertionTotal.sql
@@ -38,6 +38,7 @@ VALUES ('Potion de bouclier','Vin','474','Marko'),
 ('Potion des ténèbres','Vin','658','MagicLord');
 COMMIT;
 
+
 START TRANSACTION;
 INSERT INTO onguentingredient (IDingredient,IDonguent)
 VALUES ('1','1'),
@@ -56,4 +57,26 @@ VALUES ('1','1'),
 ('9','5'),
 ('2','5'),
 ('7','5');
+COMMIT;
+
+
+START TRANSACTION;
+INSERT INTO potioningredient (IDingredient,IDpotion)
+VALUES ('9','1'),
+('3','1'),
+('6','1'),
+('1','2'),
+('7','2'),
+('4','2'),
+('13','3'),
+('9','3'),
+('5','3'),
+('6','3'),
+('1','4'),
+('11','4'),
+('10','4'),
+('9','5'),
+('2','5'),
+('7','5'),
+('1','5');
 COMMIT;
